1c3e788557574c382b5a39b801c13fe543ee38c5
Matt Lee <matt.lee@upstatement.com>
2016-06-17 16:31:28 -0400

    add php and twig for home

---
+++ wp-content/themes/timber-practice-1/home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+Timber::render( 'home.twig', $context );
